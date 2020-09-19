b63cbf5f07a552cff5b6e93f8c22de84b1e18b9a
Pig <pig.priv@gmail.com>
2020-09-19 08:44:33 +0800

    sm6250-common: Drop no longer existed BluetoothQti
    Change-Id: I6bf3e1dda6fe5dc66f6fafdb32a1daecb9616c84
    
    Signed-off-by: SonalSingh18 <sonal.singh.19993@gmail.com>

--- common.mk
+++ common.mk
@@ -95,8 +95,7 @@ PRODUCT_PACKAGES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     audio.bluetooth.default \
-    libbthost_if \
-    BluetoothQti
+    libbthost_if
 
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.0-impl \
