af82f2ad32c79b4d4415fe363b6ae6cf74f03976
Sonal Singh <sonal.singh.19993@gmail.com>
2022-03-23 15:49:27 +0000

    sm6250-common: Convert all overlays into Runtime Resource Overlays
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

--- common.mk
+++ common.mk
@@ -291,14 +291,19 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
 
-# Notch
-PRODUCT_PACKAGES += \
-    NotchBarKiller
-
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-arrow
+PRODUCT_PACKAGES += \
+   CarrierConfigOverlay \
+   DialerOverlay \
+   FrameworksResOverlay \
+   NotchBarKillerOverlay \
+   SettingsOverlay \
+   SettingsProviderOverlay \
+   SystemUIOverlay \
+   SnapOverlay \
+   TelephonyOverlay \
+   TetheringConfigOverlay \
+   WifiOverlay
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
@@ -386,10 +391,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.qti
 
-# Tethering
-PRODUCT_PACKAGES += \
-    TetheringConfigOverlay
-
 # TextClassifier
 PRODUCT_PACKAGES += \
     textclassifier.bundle1
@@ -449,7 +450,6 @@ PRODUCT_PACKAGES += \
     hostapd \
     libwifi-hal-qcom \
     libwpa_client \
-    WifiOverlay \
     wpa_cli \
     wpa_supplicant \
     wpa_supplicant.conf
