4aa09682b3f82f3fea95d499eb55fa6f6a84a89f
Cugry <knownerx@gmail.com>
2022-11-16 14:10:38 +0900

    Revert "sm6250-common: Drop support for Snapdragon Camera"
    This reverts commit 720253fb23b5d58ec8ac850df01583d4e013aefe.

--- common.mk
+++ common.mk
@@ -119,6 +119,7 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     libgui_vendor \
+    Snap \
     GCamGOPrebuilt-V3
 
 PRODUCT_PACKAGES += \
@@ -339,6 +340,7 @@ PRODUCT_PACKAGES += \
    SettingsOverlay \
    SettingsProviderOverlay \
    SystemUIOverlay \
+   SnapOverlay \
    TelephonyOverlay \
    TetheringConfigOverlay \
    WifiOverlay
