aa331b4264d3b19cc2ad2744f18b77ca5c55db4f
Cugry <knownerx@gmail.com>
2022-11-16 14:13:47 +0900

    Revert "sm6250-common: Build GCamGOPrebuilt-V3"
    This reverts commit 418d9ef76ffa79c79b44fffc394281a8f473ebed.

--- common.mk
+++ common.mk
@@ -119,8 +119,7 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     libgui_vendor \
-    Snap \
-    GCamGOPrebuilt-V3
+    Snap
 
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
